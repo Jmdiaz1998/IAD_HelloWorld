e4176d17f53ccec05a2abeea78cab0b9222cec8c
JMDiaz <73129466+Jmdiaz1998@users.noreply.github.com>
2022-08-18 13:30:30 -0400

    Added resources and adjusted buttons
    Added resources for aeronautical engineering
    Added copy for my interest in getting my pilots license

--- Prework/app/src/main/java/com/example/intermediateandroiddevelopment_helloworld/MainActivity.kt
+++ Prework/app/src/main/java/com/example/intermediateandroiddevelopment_helloworld/MainActivity.kt
@@ -17,13 +17,20 @@ class MainActivity : AppCompatActivity() {
         helloButton.setOnClickListener {
             Log.v("Hello world", "Button clicked!")
             Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
+        }
 
-            val universityButton = findViewById<Button>(R.id.University_button)
+        val universityButton = findViewById<Button>(R.id.University_button)
             universityButton.setOnClickListener {
                 Log.v("University_Button", "University Button Clicked!")
                 findViewById<ImageView>(R.id.Main_Image).setImageResource(R.drawable.fiu_gl_and_gc)
-                findViewById<TextView>(R.id.textView).text = "Go Panthers"
+                findViewById<TextView>(R.id.textView).text = "I attend Florida International University and been doing so since 2016. Its where I earned my Bachelors degree and where soon, I will earn my Masters."
             }
+        val interestButton = findViewById<Button>(R.id.Interest_Button)
+        interestButton.setOnClickListener {
+            Log.v("Interest_Button", "Interest Button Clicked!")
+            findViewById<ImageView>(R.id.Main_Image).setImageResource(R.drawable.aeronautical_engineering)
+            findViewById<TextView>(R.id.textView).text = "I enjoy learning about aeronautical engineering and dream of getting my pilots license one day."
+        }
+
         }
     }
-}
