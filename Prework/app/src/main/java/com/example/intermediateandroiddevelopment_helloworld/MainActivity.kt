1ae99b992ae2dba9c14702b57199b053e6e96754
JMDiaz <73129466+Jmdiaz1998@users.noreply.github.com>
2022-08-18 13:11:50 -0400

    Added buttons and resources
    Added buttons for both university and interest.
    Added image of FIU and a place holder description.

--- Prework/app/src/main/java/com/example/intermediateandroiddevelopment_helloworld/MainActivity.kt
+++ Prework/app/src/main/java/com/example/intermediateandroiddevelopment_helloworld/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.intermediateandroiddevelopment_helloworld
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,10 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       val button = findViewById<Button>(R.id.Hello_Button)
-        button.setOnClickListener {
+       val helloButton = findViewById<Button>(R.id.Hello_Button)
+        helloButton.setOnClickListener {
             Log.v("Hello world", "Button clicked!")
             Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
+
+            val universityButton = findViewById<Button>(R.id.University_button)
+            universityButton.setOnClickListener {
+                Log.v("University_Button", "University Button Clicked!")
+                findViewById<ImageView>(R.id.Main_Image).setImageResource(R.drawable.fiu_gl_and_gc)
+                findViewById<TextView>(R.id.textView).text = "Go Panthers"
+            }
         }
     }
 }
